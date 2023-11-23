c0b25b821d01f007ab0e5c247e69b2aef3bd5704
lelonS <35070147+lelonS@users.noreply.github.com>
2023-11-23 11:56:31 +0100

    Scenario win

--- cypress/e2e/1-replay.feature
+++ cypress/e2e/1-replay.feature
@@ -1,8 +1,33 @@
 Feature: Replay
 
-    Scenario: Lose and play again
-        Given I am on the start page
-        When I click "Wait" until I lose
-        And I click "play again"
-        Then I should see the start page
-        And I should have more than 0 health
+  Scenario: Lose and play again
+    Given I am on the start page
+    When I click "Wait" until I lose
+    And I click "play again"
+    Then I should see the start page
+    And I should have more than 0 health
+
+  Scenario: Win and play again
+    Given I am on the start page
+    When I choose "go south"
+    And I choose "go west"
+    And I choose "wait" until I can choose "jam with the band"
+    And I choose "jam with the band"
+    And I choose "go east"
+    And I choose "go north" 2 times
+    And I choose "go east"
+    And I choose "wait" until I have "a can of beer" in my bag
+    And I choose "go west"
+    And I choose "go south"
+    And I choose "enter the cafe"
+    And I choose "buy an espresso" 3 times
+    And I choose "wait" until I can choose "give beer to barista"
+    And I choose "give beer to barista"
+    And I choose "play again"
+    Then I should see the start page
+
+  Scenario: Play the win path multiple times
+    Given I am on the start page
+    When I play the win path 20 times and save fail count
+    Then I should have less than 5 fails
+
--- cypress/e2e/step_definitions/1-replay.js
+++ cypress/e2e/step_definitions/1-replay.js
@@ -41,3 +41,67 @@ Then('I should have more than 0 health', () => {
   cy.get('.health .val').should('not.have.text', '0');
   cy.get('.health .val').should('not.have.text', '');
 });
+
+
+When('I choose {string}', (choiceText) => {
+  cy.get('.choices').contains(choiceText, { matchCase: false }).click();
+});
+
+When('I choose {string} until I can choose {string}', (choiceText, choiceToSee) => {
+  const maxLoops = 20;
+  let loopCount = 0;
+
+  function clickUntilChoice() {
+
+
+    // Make sure we don't get stuck in an infinite loop
+    loopCount++;
+    if (loopCount >= maxLoops) { throw new Error('Did not see choice. Too many loops'); }
+
+    cy.get('.choices').contains(choiceText, { matchCase: false }).click();
+
+    cy.get('.choices ul li').then((elements) => {
+      const choicesJq = elements.els[0];
+      const choices = [];
+      // Get choices text
+      for (let i = 0; i < choicesJq.length; i++) {
+        choices.push(choicesJq[i].textContent.toLowerCase());
+      }
+
+      if (choices.includes(choiceToSee.toLowerCase())) { return; }
+      clickUntilChoice();
+    });
+  }
+
+  clickUntilChoice();
+});
+
+
+When('I choose {string} {int} times', (choiceText, count) => {
+  for (let i = 0; i < count; i++) {
+    cy.get('.choices').contains(choiceText, { matchCase: false }).click();
+  }
+});
+
+When('I choose {string} until I have {string} in my bag', (choiceText, itemText) => {
+  const maxLoops = 20;
+  let loopCount = 0;
+
+  function clickUntilItem() {
+
+    // Make sure we don't get stuck in an infinite loop
+    loopCount++;
+    if (loopCount >= maxLoops) { throw new Error('Did not see item. Too many loops'); }
+
+    cy.get('.choices').contains(choiceText, { matchCase: false }).click();
+
+    cy.get('.bag-content span').then((elements) => {
+      const bagContent = elements.els[0].text().toLowerCase();
+
+      if (bagContent === itemText.toLowerCase()) { return; }
+      clickUntilItem();
+    });
+  }
+
+  clickUntilItem();
+});
