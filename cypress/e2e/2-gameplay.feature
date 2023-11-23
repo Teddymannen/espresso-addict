6a42b58a6e6a093a7a6ac33affd432313e87b171
Teddymannen <teddy_kross@hotmail.com>
2023-11-23 15:39:05 +0100

    Create tests for gameplay.feature

--- cypress/e2e/2-gameplay.feature
+++ cypress/e2e/2-gameplay.feature
@@ -22,6 +22,15 @@ Feature: Testing gameplay functions
     And I choose "wait" until I have "a can of beer" in my bag
     Then I should see "a can of beer" in "bag"
 
+  Scenario: Health bar percentage is correctly aligned with health value
+    Given I am on the start page
+    When I choose "enter the cafe"
+    And I choose "buy an espresso"
+    Then I should see "health" value at 60
+    And I should see "health" "progress" bar at "width: 60%;"
 
-# Progress Baren visar rätt andel
-# Man får alla alternativ som finns
+  Scenario: All descriptions are shown when waiting
+    Given I am on the start page
+    When I choose "north"
+    And I choose "east"
+    Then the descriptions should all be seen after I "wait" a few times
--- cypress/e2e/step_definitions/2-gameplay.js
+++ cypress/e2e/step_definitions/2-gameplay.js
@@ -1,9 +1,37 @@
 const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor');
+const { choose } = require('./support/index.js');
 
 Then('I should see {string} value at {int}', (statsClass, statsValue) => {
-    cy.get(`.${statsClass} .val`).should('have.text', `${statsValue}`);
+  cy.get(`.${statsClass} .val`).should('have.text', `${statsValue}`);
 });
 
 Then('I should see {string} in {string}', (statsContent, statsClass) => {
-    cy.get(`.${statsClass} .bag-content > span`).should('have.text', `${statsContent}`);
+  cy.get(`.${statsClass} .bag-content > span`).should('have.text', `${statsContent}`);
+});
+
+Then('I should see {string} {string} bar at {string}', (statsClass, barClass, styleWidth) => {
+  cy.get(`.${statsClass} .${barClass} > div`).should('have.attr', 'style', `${styleWidth}`);
+});
+
+Then('the descriptions should all be seen after I {string} a few times', (choiceText) => {
+  let descList = {}, count = 0;
+
+  function getDescription() {
+    choose(choiceText); // click on the 'wait' button
+
+    cy.get('.description').should('not.be.empty');
+    cy.get('.description').then(el => {
+      let descriptionText = el.els[0].text();
+      descList[descriptionText] = true;
+      count++;
+      if (Object.keys(descList).length < 3 && count < 20) {
+        getDescription();
+        return;
+      }
+      cy.log(descList);
+      expect(Object.keys(descList).length).to.equal(3);
+    });
+  }
+
+  getDescription();
 });
